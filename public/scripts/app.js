164451061fc75abbc40e06b036c69ee48e77db0e
scotpfleghaar <scot.pfleghaar@gmail.com>
2017-12-16 10:08:05 -0600

    Yarn to install dependencies

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -2,5 +2,6 @@ console.log("App.js is running");
 
 // JSX JavaScript XML
 var template = < h1 id = "someid" > This is JSX from app.js < /h1>;
+
 var appRoot = document.getElementById('app');
 ReactDOM.render(template, appRoot);
